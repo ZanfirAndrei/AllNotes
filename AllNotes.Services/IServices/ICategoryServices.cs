ab8f7ffda7a3c0e60c88d2b6d5564f336dbcd5ac
Nicolescu Bristena <bristena.nicolescu@gmail.com>
2020-02-03 00:20:16 +0200

    category CRUD, checkbox getAll

--- AllNotes.Services/IServices/ICategoryServices.cs
+++ AllNotes.Services/IServices/ICategoryServices.cs
@@ -1,12 +1,19 @@
 ﻿using AllNotes.Domain.EF.Wrapper;
+using AllNotes.Domain.Models.Sport;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AllNotes.Services.IServices
 {
     public interface ICategoryServices : IBaseServices
     {
         IWrapperRepository WrapperRepository { get; }
+        Task<IList<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task<Category> CreateAsync(string name);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> DeleteAsync(Category category);
     }
 }
---
+++ AllNotes.WebApi/Controllers/ExerciseController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllNotes.Domain.Models.Sport;
+using AllNotes.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllNotes.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly ICategoryServices _categoryServices;
+        private readonly IExerciseServices _exerciseServices;
+        private readonly ISeriesServices _seriesServices;
+
+
+        public ExerciseController(ICategoryServices categoryServices, 
+                                  IExerciseServices exerciseServices,
+                                  ISeriesServices seriesServices)
+        {
+            _categoryServices = categoryServices;
+            _exerciseServices = exerciseServices;
+            _seriesServices = seriesServices;
+        }
+
+
+        #region Categories
+        [HttpGet("GetAllCategories")]
+        public async Task<ObjectResult> GetAllCategoriesAsync()
+        {
+            IList<Category> result = await _categoryServices.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetCategory/{id}")]
+        public async Task<ObjectResult> GetCategoryAsync([FromRoute] int id)
+        {
+            Category result = await _categoryServices.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost("AddCategory")]
+        public async Task<ObjectResult> AddCategoryAsync([FromBody] string name)
+        {
+
+            Category result = await _categoryServices.CreateAsync(name);
+
+            return Ok(result);
+        }
+
+        [HttpPut("UpdateCategory/{id}")]
+        public async Task<ObjectResult> UpdateCategoryAsync([FromRoute] int id, [FromBody] string name)
+        {
+            Category result = await _categoryServices.GetByIdAsync(id);
+            if (result == null)
+            {
+                return BadRequest(new { message = "Categry not available" });
+            }
+
+            var category = new Category { Id = result.Id, Name = name};
+            await _categoryServices.UpdateAsync(category);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("DeleteCategory/{id}")]
+        public async Task<ObjectResult> DeleteCategory([FromRoute] int id)
+        {
+            Category result = await _categoryServices.GetByIdAsync(id);
+            await _categoryServices.DeleteAsync(result);
+
+            return Ok(result);
+        }
+        #endregion
+
+
+        #region Exercise
+        
+        #endregion
+
+
+        #region Series
+        
+        #endregion
+
+
+    }
+}
