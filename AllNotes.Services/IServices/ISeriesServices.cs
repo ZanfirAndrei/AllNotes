53043f2e6aef4528de43d4f91e413036160524de
Nicolescu Bristena <bristena.nicolescu@gmail.com>
2020-02-06 01:35:03 +0200

    started CRUD for series

--- AllNotes.Services/IServices/ISeriesServices.cs
+++ AllNotes.Services/IServices/ISeriesServices.cs
@@ -1,12 +1,19 @@
 ﻿using AllNotes.Domain.EF.Wrapper;
+using AllNotes.Domain.Models.Sport;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AllNotes.Services.IServices
 {
     public interface ISeriesServices : IBaseServices
     {
         IWrapperRepository WrapperRepository { get; }
+        //Task<IList<Series>> GetAllAsync();
+        //Task<Series> GetByIdAsync(int id);
+        //Task<Series> CreateAsync(string name);
+        //Task<Series> UpdateAsync(Series series);
+        //Task<Series> DeleteAsync(Series series);
     }
 }
